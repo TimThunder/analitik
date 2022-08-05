9f3aa164e8ac8ff31407cc8267b0c595a976220c
Tim Thunder <grozovsky@gmail.com>
2022-08-05 16:18:32 +0300

    now will synchronize to github

---
+++ print(input()).py
@@ -0,0 +1,13 @@
+print(input())
+
+
+def somefunc(a, b_renamed) -> int:
+    if a == 1:
+        if b_renamed == 2:
+            pass
+        else:
+            pass
+    return(a+b_renamed)
+
+
+print(somefunc(1, 2))
